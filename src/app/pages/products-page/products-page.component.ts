ec2783f731254bd1f2eff1874bdf5edacceaad09
Jan Kolkmeier <jankolkmeier@gmail.com>
2024-05-30 09:57:37 +0000

    add tables to test pages

--- src/app/pages/products-page/products-page.component.ts
+++ src/app/pages/products-page/products-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
+import { TableComponent } from '../../baader-table/components/table/table.component';
+
 @Component({
   selector: 'app-products-page',
   standalone: true,
-  imports: [],
+  imports: [TableComponent],
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.css'
 })
 export class ProductsPageComponent {
+
+  products_data_url = "/data/products.json";
+  products_display_columns = ["id", "title", "category", "price"];
 
 }
--- src/app/pages/users-page/users-page.component.ts
+++ src/app/pages/users-page/users-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { TableComponent } from '../../baader-table/components/table/table.component';
 
 @Component({
   selector: 'app-users-page',
   standalone: true,
-  imports: [],
+  imports: [TableComponent],
   templateUrl: './users-page.component.html',
   styleUrl: './users-page.component.css'
 })
 export class UsersPageComponent {
-
+  users_data_url = "/data/users.json";
+  users_display_columns = ["name.first", "name.last", "dob.age", "location.country", "location.state", "location.street.name", "location.street.number"];
 }
