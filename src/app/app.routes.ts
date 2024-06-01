0c46264eb21f24b249e15462700e26c6fc2021b5
Jan Kolkmeier <jankolkmeier@gmail.com>
2024-06-01 10:06:45 +0000

    Create a test page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AssignmentPageComponent } from './pages/assignment-page/assignment-page.component';
 import { UsersPageComponent } from './pages/users-page/users-page.component';
 import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { TestPageComponent } from './pages/test-page/test-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/assignment', pathMatch: 'full' },
     { path: 'assignment', component: AssignmentPageComponent },
+    { path: 'test', component: TestPageComponent },
     { path: 'users', component: UsersPageComponent },
     { path: 'products', component: ProductsPageComponent }
 ];
---
+++ src/app/pages/test-page/test-page.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { TableComponent } from '../../baader-table/components/table/table.component';
+import { ColumnSpec } from '../../baader-table/shared/table.model';
+
+interface TableSettings {
+  url: string,
+  columns: ColumnSpec[]
+}
+
+@Component({
+  selector: 'app-test-page',
+  standalone: true,
+  imports: [TableComponent],
+  templateUrl: './test-page.component.html',
+  styleUrl: './test-page.component.css'
+})
+export class TestPageComponent {
+
+  tables = [
+    {
+      url: "/data/products.json",
+      columns: [
+        { name: "id", displayName: "ID" },
+        { name: "title", displayName: "Title" },
+        { name: "category", displayName: "Category" },
+        { name: "price", displayName: "Price" },
+      ]
+    }, {
+      url: "/data/users.json",
+      columns: [
+        { name: "name.first", displayName: "First Name" },
+        { name: "name.last", displayName: "Last Name" },
+        { name: "dob.age", displayName: "Age" },
+        { name: "location.country", displayName: "Country" },
+        { name: "location.state", displayName: "State" },
+        { name: "location.street.name", displayName: "Street Name" },
+        { name: "location.street.number", displayName: "Street Number" }
+      ]
+    }
+
+  ] as TableSettings[];
+
+  thirdTableIdx = 0;
+
+  cycleThirdTable() {
+    this.thirdTableIdx = (this.thirdTableIdx + 1) % this.tables.length;
+  }
+
+}
